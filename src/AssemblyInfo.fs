694cd40200dac74f1ae49b1792234dae521e5bda
Team City <info@exira.com>
2016-01-05 21:14:27 +0000

    Bump version to 0.1.4

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("aws-s3-cache-crawler")>]
 [<assembly: AssemblyProductAttribute("Exira.AwsS3Cache.Crawler")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.AwsS3Cache.Crawler is a task which crawls a site so pages are pre-loaded into cache")>]
-[<assembly: AssemblyVersionAttribute("0.1.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.3")>]
-[<assembly: AssemblyMetadataAttribute("githash","f1909b1d73575e3ca7fd24d212bcb7cdec8d6f2f")>]
+[<assembly: AssemblyVersionAttribute("0.1.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.4")>]
+[<assembly: AssemblyMetadataAttribute("githash","49b248d0254eb80d498f5739ee8f4d60a8aacc2a")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.3"
+    let [<Literal>] Version = "0.1.4"
